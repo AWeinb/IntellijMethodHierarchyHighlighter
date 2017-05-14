24355b5dc3e28750b51428a3316521805e3ef671
AWeinb <AWeinb@t-online.de>
2017-05-14 16:24:52 +0200

    Assert that the handled method is in same PsiFile

--- src/de/axp/hierarchyhighlighter/MethodHighlighter.java
+++ src/de/axp/hierarchyhighlighter/MethodHighlighter.java
@@ -3,11 +3,15 @@ package de.axp.hierarchyhighlighter;
 import com.google.common.collect.Sets;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.RangeMarker;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
 import de.axp.hierarchyhighlighter.MethodBackgroundPainter.PaintType;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 class MethodHighlighter {
@@ -26,7 +30,7 @@ class MethodHighlighter {
 		methodBackgroundPainter.getPaintJobs().forEach(RangeMarker::dispose);
 		methodBackgroundPainter.getPaintJobs().clear();
 
-		if (isValid(psiElement)) {
+		if (isValid(psiElement) && isInSameFile(editor, psiElement)) {
 			PsiMethod psiMethod = (PsiMethod) psiElement;
 			Set<PsiMethod> parentMethods = methodFinder.findMethodCallsOf(psiMethod);
 			Set<PsiMethod> childMethods = methodFinder.findAllChildMethodsOf(psiMethod);
@@ -39,6 +43,15 @@ class MethodHighlighter {
 
 	private boolean isValid(PsiElement psiElement) {
 		return psiElement != null && psiElement instanceof PsiMethod;
+	}
+
+	private boolean isInSameFile(Editor editor, PsiElement psiElement) {
+		Project project = editor.getProject();
+		if (project != null) {
+			PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
+			return Objects.equals(psiFile, psiElement.getContainingFile());
+		}
+		return false;
 	}
 
 	private void foldMethodsInFile(Editor editor, PsiMethod psiMethod) {
